f4d129ce9b1f02820c0e7e2621c119db49ecb936
Yusuf Semih Sancar <126322108+YusufSemihSancar@users.noreply.github.com>
2023-07-17 12:50:32 +0300

    MyWebSites

---
+++ Word Guessing Game/Word Guessing Game/js/words.js
@@ -0,0 +1,198 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "Programlama dili"
+    },
+    {
+        word: "guitar",
+        hint: "müzik enstrümanı"
+    },
+    {
+        word: "aim",
+        hint: "bir amaç veya niyet"
+    },
+    {
+        word: "venus",
+        hint: "güneş sistemimizin gezegeni"
+    },
+    {
+        word: "gold",
+        hint: "sarı değerli metal"
+    },
+    {
+        word: "ebay",
+        hint: "çevrimiçi alışveriş sitesi"
+    },
+    {
+        word: "golang",
+        hint: "Programlama dili"
+    },
+    {
+        word: "coding",
+        hint: "programlama ile ilgili"
+    },
+    {
+        word: "matrix",
+        hint: "Bilim kurgu filmi"
+    },
+    {
+        word: "bugs",
+        hint: "programlama ile ilgili"
+    },
+    {
+        word: "avatar",
+        hint: "epik bilim kurgu filmi"
+    },
+    {
+        word: "gif",
+        hint: "görüntü için bir dosya formatı"
+    },
+    {
+        word: "mental",
+        hint: "zihin ile ilgili"
+    },
+    {
+        word: "map",
+        hint: "diyagram bir alanı temsil eder"
+    },
+    {
+        word: "island",
+        hint: "Arazi su ile çevrili"
+    },
+    {
+        word: "hockey",
+        hint: "ünlü bir açık hava oyunu"
+    },
+    {
+        word: "chess",
+        hint: "masa oyunu"
+    },
+    {
+        word: "viber",
+        hint: "bir sosyal medya uygulaması"
+    },
+    {
+        word: "github",
+        hint: "kod barındırma platformu"
+    },
+    {
+        word: "png",
+        hint: "bir resim dosyası formatı"
+    },
+    {
+        word: "silver",
+        hint: "değerli grimsi beyaz metal"
+    },
+    {
+        word: "mobile",
+        hint: "bir elektronik cihaz"
+    },
+    {
+        word: "gpu",
+        hint: "bilgisayar bileşeni"
+    },
+    {
+        word: "java",
+        hint: "Programlama dili"
+    },
+    {
+        word: "google",
+        hint: "ünlü arama motoru"
+    },
+    {
+        word: "venice",
+        hint: "ünlü sular şehri"
+    },
+    {
+        word: "excel",
+        hint: "windows için microsoft ürünü"
+    },
+    {
+        word: "mysql",
+        hint: "ilişkisel veritabanı sistemi"
+    },
+    {
+        word: "nepal",
+        hint: "gelişmekte olan ülke adı"
+    },
+    {
+        word: "flute",
+        hint: "müzik enstrümanı"
+    },
+    {
+        word: "crypto",
+        hint: "kripto para birimi"
+    },
+    {
+        word: "tesla",
+        hint: "araba markası"
+    },
+    {
+        word: "mars",
+        hint: "güneş sistemimizin gezegeni"
+    },
+    {
+        word: "proxy",
+        hint: "sunucu uygulamasıyla ilgili"
+    },
+    {
+        word: "email",
+        hint: "mesaj alışverişi ile ilgili"
+    },
+    {
+        word: "html",
+        hint: "web için biçimlendirme dili"
+    },
+    {
+        word: "air",
+        hint: "bir gazla ilgili"
+    },
+    {
+        word: "idea",
+        hint: "bir düşünce veya öneri"
+    },
+    {
+        word: "server",
+        hint: "bilgisayar veya sistemle ilgili"
+    },
+    {
+        word: "svg",
+        hint: "bir vektör görüntü formatı"
+    },
+    {
+        word: "jpeg",
+        hint: "bir resim dosyası formatı"
+    },
+    {
+        word: "search",
+        hint: "bir şey bulmak için harekete geç"
+    },
+    {
+        word: "key",
+        hint: "küçük metal parçası"
+    },
+    {
+        word: "egypt",
+        hint: "bir ülke adı"
+    },
+    {
+        word: "joker",
+        hint: "psikolojik gerilim filmi"
+    },
+    {
+        word: "dubai",
+        hint: "gelişmiş ülke adı"
+    },
+    {
+        word: "photo",
+        hint: "kişi veya sahnenin temsili"
+    },
+    {
+        word: "nile",
+        hint: "dünyanın en büyük nehri"
+    },
+    {
+        word: "rain",
+        hint: "hava olayı"
+    },
+]
